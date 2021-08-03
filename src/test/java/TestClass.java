ca8eb851ed096b38b4f7a0fee2ccda2113ccc2e0
Slava Shneidmillier <slava.shneidmillier@exabeam.com>
2021-08-03 12:53:58 +0300

    first

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
     @Test
     void test() {
-        int actualSum = HelloWorld.sum(2, 3);
+        int actualSum = HelloWorld.sum(2, 8);
         assert actualSum == 5;
         System.out.println("actual sum is :" + actualSum);
     }
